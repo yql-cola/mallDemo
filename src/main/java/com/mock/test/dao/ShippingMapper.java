5bb995fd6eb733c06cb95bffe302a3121f331e12
yql <158170425@qq.com>
2021-10-23 18:39:15 +0800

    shipping generator commit

---
+++ src/main/java/com/mock/test/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.mock.test.dao;
+
+import com.mock.test.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
